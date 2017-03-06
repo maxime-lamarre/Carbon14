afcc02b0e9493a0ab82f707edd37259b88d06cda
Unknown <vincent.deguille@live.fr>
2017-03-06 15:45:57 -0500

    Fichiers de migrations créés et fonctionnels.

---
+++ carbon14-web/database/migrations/2017_03_06_203553_create_time_entries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimeEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('time_entries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('duration');
+            $table->bigInteger('task_id');
+            $table->bigInteger('time_sheet_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('time_entries');
+    }
+}
